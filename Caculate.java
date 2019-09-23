468749944a95df1de8d17f4361a3fd5d4baa5756
Your Name <you@example.com>
2019-09-23 20:00:18 +0800

    Code

---
+++ Caculate.java
@@ -0,0 +1,14 @@
+public class Caculate{
+	public static void main(String[] args){
+		System.out.println("34 + 30 = " + 30 + 40);  //???????
+		
+		int n1 = 3;
+	    int n2 = 5;
+	
+	    double result = (double)(n1/n2);
+		
+		System.out.printf(" result = %.2f",result);
+		
+	}
+		
+}
